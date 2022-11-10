ab727a4c2ce620b7726c0380bf1dac68f3f74428
Hafiz <hafiz.sust333@gmail.com>
2022-11-10 09:12:46 +0600

    Name And logo was changed and Updated by Hafiz

--- REACH_main/app/src/main/java/com/hafiz/reach_main_10/login_activity.java
+++ REACH_main/app/src/main/java/com/hafiz/reach_main_10/login_activity.java
@@ -2,7 +2,10 @@ package com.hafiz.reach_main_10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class login_activity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class login_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView tb = findViewById(R.id.GoToSignup);
+
+        tb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(login_activity.this , signup_activity.class);
+                startActivity(in);
+            }
+        });
     }
 }
