3738c4d3291014e9834d21579faa91844cc26895
Ashrafur Rahman Chowdhury <74958102+SaYMy-NaMe@users.noreply.github.com>
2022-12-11 13:56:54 +0600

    Updated test by Ashraf

--- REACH_main/app/src/androidTest/java/com/hafiz/reach_main_10/ExampleInstrumentedTest.java
+++ REACH_main/app/src/androidTest/java/com/hafiz/reach_main_10/ExampleInstrumentedTest.java
@@ -24,3 +24,22 @@ public class ExampleInstrumentedTest {
         assertEquals("com.hafiz.reach_main_10", appContext.getPackageName());
     }
 }
+
+/*
+<?xml version="1.0" encoding="utf-8"?>
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:tools="http://schemas.android.com/tools">
+
+    <application
+        android:allowBackup="true"
+        android:dataExtractionRules="@xml/data_extraction_rules"
+        android:fullBackupContent="@xml/backup_rules"
+        android:icon="@mipmap/ic_launcher"
+        android:label="@string/app_name"
+        android:roundIcon="@mipmap/ic_launcher_round"
+        android:supportsRtl="true"
+        android:theme="@style/Theme.Activity_car_washing"
+        tools:targetApi="31" />
+
+</manifest>
+*/
