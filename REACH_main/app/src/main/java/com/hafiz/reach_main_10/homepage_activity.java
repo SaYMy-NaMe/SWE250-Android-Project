4459086ace55ba8b4399ba3c29ffe217dabcbfd6
Hafiz <hafiz.sust333@gmail.com>
2022-12-26 04:10:37 +0600

    Menu Option and Profile.xml file added by Hafiz

--- REACH_main/app/src/main/java/com/hafiz/reach_main_10/homepage_activity.java
+++ REACH_main/app/src/main/java/com/hafiz/reach_main_10/homepage_activity.java
@@ -1,17 +1,72 @@
 package com.hafiz.reach_main_10;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.logging.Logger;
 
 public class homepage_activity extends AppCompatActivity {
 
+
+    // For Option menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater mi = getMenuInflater();
+        mi.inflate(R.menu.popup_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        if(id == R.id.profile)
+        {
+            // go to profile
+            startActivity(new Intent(homepage_activity.this, profile_activity.class));
+
+        }
+        if(id == R.id.profile)
+        {
+
+        }
+        return true;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         
         setContentView(R.layout.activity_homepage);
+
+
+
+
+        ImageView im = findViewById(R.id.plunmbing);
+
+
+        im.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(homepage_activity.this , plumbing_activity.class);
+                startActivity(in);
+            }
+        });
+
+
     }
 }
