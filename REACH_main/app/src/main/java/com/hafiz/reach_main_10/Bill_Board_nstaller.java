58e6be606f0805a037289f2c3d0c29fbda380ba9
ashraf <ashrafurrahman42069@gmail.com>
2023-02-05 00:50:21 +0600

    Bill Board Done

---
+++ REACH_main/app/src/main/java/com/hafiz/reach_main_10/Bill_Board_nstaller.java
@@ -0,0 +1,51 @@
+package com.hafiz.reach_main_10;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.hafiz.reach_main_10.databinding.ActivityBillBoardNstallerBinding;
+
+public class Bill_Board_nstaller extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityBillBoardNstallerBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityBillBoardNstallerBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_bill_board_nstaller);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_bill_board_nstaller);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
