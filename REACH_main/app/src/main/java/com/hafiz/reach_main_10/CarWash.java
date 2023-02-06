5daee197f9a45917ceca4d0e45424f1189c60896
Hafiz <hafiz.sust333@gmail.com>
2023-02-06 12:34:11 +0600

    Two acticity file connected with homepage

--- REACH_main/app/src/main/java/com/hafiz/reach_main_10/CarWash.java
+++ REACH_main/app/src/main/java/com/hafiz/reach_main_10/CarWash.java
@@ -2,7 +2,10 @@ package com.hafiz.reach_main_10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CarWash extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class CarWash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_wash);
+
+        Button b = findViewById(R.id.back3);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(CarWash.this , homepage_activity.class);
+                startActivity(in);
+            }
+        });
     }
 }
