2b60cf70686569c062f567951aed107f9b33a0ed
sanjana54 <safrinmemon@gmail.com>
2023-02-10 21:45:32 +0600

    activity_solar_energy_consultant

---
+++ REACH_main/app/src/main/java/com/hafiz/reach_main_10/SolarEnergyConsultantActivity.java
@@ -0,0 +1,14 @@
+package com.hafiz.reach_main_10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SolarEnergyConsultantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solar_energy_consultant);
+    }
+}
