2a370efa3539bb37334464bee84b23b56c288ab4
sanjana54 <safrinmemon@gmail.com>
2023-02-10 14:29:57 +0600

    activity_gym_equipment_installer

--- REACH_main/app/src/main/java/com/hafiz/reach_main_10/GymEquipmentInstallerActivity.java
+++ REACH_main/app/src/main/java/com/hafiz/reach_main_10/GymEquipmentInstallerActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ExerciseEquipmentInstallerActivity extends AppCompatActivity {
+public class GymEquipmentInstallerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exercise_equipment_installer);
+        setContentView(R.layout.activity_gym_equipment_installer);
     }
 }
