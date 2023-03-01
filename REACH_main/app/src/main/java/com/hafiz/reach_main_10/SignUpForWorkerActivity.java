b87f44134929b93232d90b270b8d3a0a3f45fe15
ashraf <ashrafurrahman42069@gmail.com>
2023-03-01 15:30:14 +0600

    Sign up for Worker Done

---
+++ REACH_main/app/src/main/java/com/hafiz/reach_main_10/SignUpForWorkerActivity.java
@@ -0,0 +1,14 @@
+package com.hafiz.reach_main_10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpForWorkerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up_for_worker);
+    }
+}
