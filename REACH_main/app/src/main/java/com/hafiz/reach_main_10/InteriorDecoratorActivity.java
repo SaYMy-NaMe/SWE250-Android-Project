f1681275e201bcea15efc8a24883e82a2690910c
Hafiz <hafiz.sust333@gmail.com>
2023-03-09 19:00:20 +0600

    All pages Connected with Homepage

--- REACH_main/app/src/main/java/com/hafiz/reach_main_10/InteriorDecoratorActivity.java
+++ REACH_main/app/src/main/java/com/hafiz/reach_main_10/InteriorDecoratorActivity.java
@@ -2,7 +2,11 @@ package com.hafiz.reach_main_10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class InteriorDecoratorActivity extends AppCompatActivity {
 
@@ -10,5 +14,64 @@ public class InteriorDecoratorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interior_decorator);
+
+        Button b = findViewById(R.id.back3);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(InteriorDecoratorActivity.this, homepage_activity.class);
+                startActivity(in);
+            }
+        });
+        Button b1 = findViewById(R.id.Confirm);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(InteriorDecoratorActivity.this,InteriorDecorationConfirmActivity.class));
+            }
+        });
+
+        //MenuBar
+
+
+        ImageView home = findViewById(R.id.Home_home);
+        ImageView prof = findViewById(R.id.Home_prof);
+        ImageView cart = findViewById(R.id.home_cart);
+        ImageView supp = findViewById(R.id.Home_Supp);
+        ImageView sett = findViewById(R.id.Home_sett);
+
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(InteriorDecoratorActivity.this,homepage_activity.class));
+
+            }
+        });
+        prof.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(InteriorDecoratorActivity.this,profile_activity.class));
+            }
+        });
+        cart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(InteriorDecoratorActivity.this,CartListActivity.class));
+            }
+        });
+        supp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(InteriorDecoratorActivity.this,Support_activity.class));
+            }
+        });
+        sett.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(InteriorDecoratorActivity.this,Settings_activity.class));
+            }
+        });
     }
 }
