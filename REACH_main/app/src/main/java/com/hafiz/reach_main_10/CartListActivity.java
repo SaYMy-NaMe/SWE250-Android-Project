fe1e670836edbe965826b5852c239e7689cab329
ashraf <ashrafurrahman42069@gmail.com>
2023-03-15 16:05:37 +0600

    Error resolved

--- REACH_main/app/src/main/java/com/hafiz/reach_main_10/CartListActivity.java
+++ REACH_main/app/src/main/java/com/hafiz/reach_main_10/CartListActivity.java
@@ -2,7 +2,10 @@ package com.hafiz.reach_main_10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class CartListActivity extends AppCompatActivity {
 
@@ -10,5 +13,46 @@ public class CartListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart_list);
+        //MenuBar
+
+
+        ImageView home = findViewById(R.id.Home_home);
+        ImageView prof = findViewById(R.id.Home_prof);
+        ImageView cart = findViewById(R.id.home_cart);
+        ImageView supp = findViewById(R.id.Home_Supp);
+        ImageView sett = findViewById(R.id.Home_sett);
+
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CartListActivity.this,homepage_activity.class));
+
+            }
+        });
+        prof.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CartListActivity.this,profile_activity.class));
+            }
+        });
+        cart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CartListActivity.this,CartListActivity.class));
+            }
+        });
+        supp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CartListActivity.this,SupportpageDetailsActivity.class));
+            }
+        });
+        sett.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CartListActivity.this,SettingsInternalActivity.class));
+            }
+        });
     }
 }
